e0b59465db6e652516992a1b12b477b76eed656a
Prosper Atu <atu.prosper@gmail.com>
2024-09-17 00:40:23 +0100

    created 2D rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Rotates 2D matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates 2D matrix"""
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            top_left = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = top_left
+        right -= 1
+        left += 1
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,1 @@
+ALX Interview - 2D matrix
