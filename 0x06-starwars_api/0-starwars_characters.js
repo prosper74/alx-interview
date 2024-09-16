dbd1b41972562298f733fd1f5e36fb52ea72bc52
Prosper Atu <atu.prosper@gmail.com>
2024-09-17 00:26:13 +0100

    fixed semistd error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 const movieId = process.argv[2];
-const filmEndPoint = "https://swapi-api.hbtn.io/api/films/" + movieId;
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 let people = [];
 const names = [];
 
@@ -11,7 +11,7 @@ const requestCharacters = async () => {
   await new Promise((resolve) =>
     request(filmEndPoint, (err, res, body) => {
       if (err || res.statusCode !== 200) {
-        console.error("Error: ", err, "| StatusCode: ", res.statusCode);
+        console.error('Error: ', err, '| StatusCode: ', res.statusCode);
       } else {
         const jsonBody = JSON.parse(body);
         people = jsonBody.characters;
@@ -27,7 +27,7 @@ const requestNames = async () => {
       await new Promise((resolve) =>
         request(p, (err, res, body) => {
           if (err || res.statusCode !== 200) {
-            console.error("Error: ", err, "| StatusCode: ", res.statusCode);
+            console.error('Error: ', err, '| StatusCode: ', res.statusCode);
           } else {
             const jsonBody = JSON.parse(body);
             names.push(jsonBody.name);
@@ -37,7 +37,7 @@ const requestNames = async () => {
       );
     }
   } else {
-    console.error("Error: Got no Characters for some reason");
+    console.error('Error: Got no Characters for some reason');
   }
 };
 
@@ -49,7 +49,7 @@ const getCharNames = async () => {
     if (n === names[names.length - 1]) {
       process.stdout.write(n);
     } else {
-      process.stdout.write(n + "\n");
+      process.stdout.write(n + '\n');
     }
   }
 };
